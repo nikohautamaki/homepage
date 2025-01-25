8c21c84b669fc2ddf10b767ba2544b3f34b920c7
Miro Tammi <mirotammi@Mac.home>
2025-01-25 14:56:09 +0200

    working site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router components
+import { Route, Routes } from 'react-router-dom'; // Remove the BrowserRouter import
 import './App.css'; // Add styling here
 import profileImage from './images/niko1.jpg';
 import Header from './components/Header';
@@ -11,22 +11,20 @@ import WorkExamplesPage from './components/WorkExamplesPage'; // Import WorkExam
 
 function App() {
   return (
-    <Router>  {/* Wrap the app in Router for routing functionality */}
-      <div className="App">
-        <Header />  {/* This will render the header with navigation links */}
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Main />
-              {/* <WorkExamplesPage />  */}
-            </>
-          } />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Footer />  {/* This will render the footer */}
-      </div>
-    </Router>
+    <div className="App">
+      <Header />  {/* This will render the header with navigation links */}
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Main />
+            {/* <WorkExamplesPage />  */}
+          </>
+        } />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />  {/* This will render the footer */}
+    </div>
   );
 }
 
